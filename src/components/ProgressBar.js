06e67d68681d89389566418a82c1b9edaba8732f
manjot <manjot.simbacart@gmail.com>
2018-07-10 17:04:08 +0530

    fixed bugs

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -7,11 +7,11 @@ export default class ProgressBar extends Component {
     return (
         <div className='progressBarContainer'>
             <div style={{    overflow: 'hidden'}}>
-                <h3 style={{fontSize:'1.4vmax',textAlign:'center',color:'black','white-space': 'nowrap'}}>
+                <h3 style={{fontSize:'1.5vmin',textAlign:'center',color:'black','white-space': 'nowrap'}}>
                     {this.props.skillName}
                 </h3>
-            </div>
             <Circle trailColor='grey' percent={this.props.progress} strokeWidth="10" strokeColor="#D3D3D3"  />
+            </div>
 
         </div>
 
